f22f9db3635dfa83ea99604c239cd33b0d0fb800
mudungazey.macou@gmail.com <dd.yazalde@gmail.com>
2016-07-18 13:40:41 +0000

    Yazalde, Damasio | adding seller page and stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "welcome#index"
+
+  resources :sales
+
+  get '/buyer' => 'sales#buyer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
