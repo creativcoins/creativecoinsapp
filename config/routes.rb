3c6054c8e2da2f323112e8516f42297840264db5
Rim20 <cho.rimsky@gmail.com>
2016-07-19 09:36:16 +0530

    add navigation info | testimony | to views/welcome

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 	resources :sales
 	resources :purchases
 
+	get '/info' => 'welcome#info'
+	get '/testimony' => 'welcome#testimony'
+
   devise_for :users
   root "welcome#index"
 
